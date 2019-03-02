9ae9c3e9ab12d532fe4329af7993e261ee327b56
akl <koleda.andrey@gmail.com>
2019-03-02 21:39:07 +0300

    RFM service

--- src/Foundation/BusinessValueTracker/code/Services/MLService.cs
+++ src/Foundation/BusinessValueTracker/code/Services/MLService.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Hackathon.Boilerplate.Foundation.BusinessValueTracker.Mappers;
+using Hackathon.Boilerplate.Foundation.BusinessValueTracker.Models;
 using Hackathon.Boilerplate.Foundation.BusinessValueTracker.Models.Clients;
 using Hackathon.Boilerplate.Foundation.BusinessValueTracker.Models.Cortex;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Sitecore.ContentTesting.ML.Workers;
 using Sitecore.Processing.Engine.ML.Abstractions;
 using Sitecore.Processing.Engine.Projection;
 using Sitecore.Processing.Engine.Storage.Abstractions;
@@ -15,40 +18,60 @@ namespace Hackathon.Boilerplate.Foundation.BusinessValueTracker.Services
 {
     public class MLService : BaseWorker, IMLService
     {
-        private readonly ITableStoreFactory _tableStoreFactory;
         private readonly ILogger<MLService> _logger;
+        private readonly ITableStoreFactory _tableStoreFactory;
+        private readonly RfmCalculateService _rfmCalculateService;
+        private static List<Client> _clients;
 
-        private readonly double _testValue;
-        private RfmCalculateService _rfmCalculateService;
+        public MLService(ITableStoreFactory tableStoreFactory, IConfiguration configuration, ILogger<MLService> logger)
+            : base(tableStoreFactory)
+        {
+            _tableStoreFactory = tableStoreFactory;
+            _logger = logger;
+            _rfmCalculateService = new RfmCalculateService();
+        }
 
-        public Task<ModelStatistics> Train(string schemaName, CancellationToken cancellationToken, params TableDefinition[] tables)
+        public async Task<ModelStatistics> Train(string schemaName, CancellationToken cancellationToken,
+            params TableDefinition[] tables)
         {
             _logger.LogInformation("Executing Train method of MLNetService for table with schema=" + schemaName);
+
             var tableStore = _tableStoreFactory.Create(schemaName);
             var data = await GetDataRowsAsync(tableStore, tables.First().Name, cancellationToken);
 
-            var calculatedScores = _rfmCalculateService.CalculateRfmScores(ClientMapper. MapToClients(data));
+            var calculatedScores = _rfmCalculateService.CalculateRfmScores(ClientMapper.MapToClients(data));
 
-            var businessData = calculatedScores.Select(x => new Rfm
-            {
-                R = x.R,
-                F = x.F,
-                M = x.M
-            }).ToList();
+            _clients = calculatedScores;
 
-            return new RfmStatistics { Clients = calculatedScores };
+            return new RfmStatistics {Clients = calculatedScores};
         }
 
-        public Task<IReadOnlyList<object>> Evaluate(string schemaName, CancellationToken cancellationToken, params TableDefinition[] tables)
+        public async Task<IReadOnlyList<object>> Evaluate(string schemaName, CancellationToken cancellationToken,
+            params TableDefinition[] tables)
         {
-            throw new NotImplementedException();
+            var tableStore = _tableStoreFactory.Create(schemaName);
+            var data = await GetDataRowsAsync(tableStore, tables.First().Name, cancellationToken);
+
+            var results = new List<PredictionResult>();
+
+            foreach (var dataRow in data)
+            {
+                if (dataRow.Enabled())
+                {
+
+                }
+            }
+
+            return results;
         }
     }
 
     public interface IMLService
     {
-        Task<ModelStatistics> Train(string schemaName, CancellationToken cancellationToken, params TableDefinition[] tables);
+        Task<ModelStatistics> Train(string schemaName, CancellationToken cancellationToken,
+            params TableDefinition[] tables);
 
-        Task<IReadOnlyList<object>> Evaluate(string schemaName, CancellationToken cancellationToken, params TableDefinition[] tables);
+        Task<IReadOnlyList<object>> Evaluate(string schemaName, CancellationToken cancellationToken,
+            params TableDefinition[] tables);
     }
 }
