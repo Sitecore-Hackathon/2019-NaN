869eca1ad0f36e6c4a94c71ab839b4d4ee98e819
x3mxray <x3m.xray@gmail.com>
2019-03-02 18:38:02 +0300

    ClusterMatch

---
+++ src/Foundation/BusinessValueTracker/code/Conditionals/ClusterMatch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq.Expressions;
+using Hackathon.Boilerplate.Foundation.BusinessValueTracker.Facets;
+using Sitecore.Framework.Rules;
+using Sitecore.XConnect;
+using Sitecore.XConnect.Segmentation.Predicates;
+
+namespace Hackathon.Boilerplate.Foundation.BusinessValueTracker.Conditionals
+{
+    public class ClusterMatch : ICondition, IContactSearchQueryFactory
+    {
+        public NumericOperationType Comparison { get; set; }
+
+        public int Number { get; set; }
+        public bool Evaluate(IRuleExecutionContext context)
+        {
+            var contact = context.Fact<Contact>();
+            var facet = contact.GetFacet<RfmContactFacet>(RfmContactFacet.DefaultFacetKey);
+            if (facet == null) return false;
+            int cluster = facet.Cluster;
+
+            return Comparison.Evaluate(cluster, Number);
+        }
+
+        public Expression<Func<Contact, bool>> CreateContactSearchQuery(IContactSearchQueryContext context)
+        {
+            return contact => Comparison.Evaluate(contact.GetFacet<RfmContactFacet>(RfmContactFacet.DefaultFacetKey).R, Number);
+        }
+    }
+}
---
+++ src/Foundation/BusinessValueTracker/code/Conditionals/RfmMatch.cs
@@ -0,0 +1,8 @@
+﻿namespace Hackathon.Boilerplate.Foundation.BusinessValueTracker.Conditionals
+{
+    public class PredictionResult
+    {
+        public string Email { get; set; }
+        public int Cluster { get; set; }
+    }
+}
